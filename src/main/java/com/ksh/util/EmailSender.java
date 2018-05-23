89365fcfc51f9a282b82dbbffa635e11a6e41846
Kshitij <Kshitij.Solanki@rishabhsoft.com>
2018-05-23 11:38:25 +0100

    updated...

--- src/main/java/com/ksh/util/EmailSender.java
+++ src/main/java/com/ksh/util/EmailSender.java
@@ -23,8 +23,8 @@ public class EmailSender
 		String host = "smtp.gmail.com";
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", host);
-		props.put("mail.smtp.user", "solanki078@gmail.com");
-		props.put("mail.smtp.password", "Lilykinjal2312*");
+		props.put("mail.smtp.user", "xitij9092@gmail.com");
+		props.put("mail.smtp.password", "Temp23Temp*");
 		props.put("mail.smtp.port", "587");
 		props.put("mail.smtp.auth", "true");
 		Session session = Session.getDefaultInstance(props);
